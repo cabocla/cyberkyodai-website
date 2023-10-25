a013743cfbb083cd9794d3505f9fab7c44ca028b
cabocla <tanhok97@gmail.com>
2023-10-25 17:13:57 +0700

    new file as API URL helper

---
+++ src/app/methods.js
@@ -0,0 +1,9 @@
+const contractApi = "/api/contracts/";
+export const kyodaiApi = contractApi + "kyodai/";
+
+export const methods = {
+  totalSupply: "total-supply",
+  allianceSupply: "alliance-supply",
+  name: "name",
+  //   publicMint: "public-mint",
+};
