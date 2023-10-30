758e196ba773e1f8568124eb1ae4bb397cba76ab
cabocla <tanhok97@gmail.com>
2023-10-30 22:22:17 +0700

    delete unused

--- src/app/api/methods.js
+++
@@ -1,9 +0,0 @@
-const contractApi = "/api/contracts/";
-export const kyodaiApi = contractApi + "kyodai/";
-
-export const methods = {
-  totalSupply: "total-supply",
-  allianceSupply: "alliance-supply",
-  name: "name",
-  //   publicMint: "public-mint",
-};
