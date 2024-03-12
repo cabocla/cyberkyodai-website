a668739187a75e62322e58a267b2ab89b88bbeb4
cabocla <tanhok97@gmail.com>
2024-03-12 21:15:39 +0700

    padding adjustment

--- src/app/components/clans.js
+++ src/app/components/clans.js
@@ -6,10 +6,10 @@ function Clans() {
   return (
     <PageContent id="clans">
       <div className="flex  h-full  flex-col-reverse items-center lg:flex-row">
-        <div className="flex flex-col lg:h-[40vw] lg:w-[40vw]">
+        <div className="flex flex-col items-center justify-center  lg:h-[40vw] lg:w-[40vw]">
           <Image src={clanLogos} alt="Cyber Kyodai Clan Logos" />
         </div>
-        <div className="mx-10 flex h-full flex-col justify-center px-20 lg:w-[50vw]">
+        <div className="mx-10 flex h-full flex-col justify-end px-10 pb-10 lg:w-[50vw] lg:justify-center lg:px-20">
           <h2 className="text-5xl">Clans</h2>
           <p>
             Within the depths of Neo Tokyo underbelly, intricate power struggle
